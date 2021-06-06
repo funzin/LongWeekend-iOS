96b9c0911174668a3c55bdb1d733fd9a364fc632
funzin <nakazawa.fumito@gmail.com>
2021-06-06 15:23:05 +0900

    Add swiftformat

---
+++ CLI/_swiftformat/Package.swift
@@ -0,0 +1,11 @@
+// swift-tools-version:5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "_swiftformat",
+    dependencies: [
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", .exact("0.48.4"))
+    ]
+)
+
